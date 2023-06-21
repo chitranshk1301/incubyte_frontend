ca35206ddb8185d3260604274c3182c5894c3e91
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-06-22 00:37:07 +0530

    final commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,33 +2,33 @@ import React from 'react'
 import '../index.css'
 
 const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="/"><span className="home">Incubyte</span></a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#about-us">About Us</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link active" href="#our-expertice">Our Expertice</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link active" href="#success-story">Success Stories</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link active" href="#contact-us">Contact Us</a>
-                    </li>
-                </ul>
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/"><span className="home text-primary-emphasis">Incubyte</span></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#about-us">About Us</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#our-expertice">Our Expertice</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#success-story">Success Stories</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#contact-us">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
